143f4864e70281fa070234b4d17ed5424d28b2a2
roberthbsantos <fco.roberth@gmail.com>
2024-02-24 13:28:54 -0300

    criando model de user e endpoit para crud

---
+++ teste_python/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from accounts.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'is_staff',
+            'is_active'
+        )
+
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True, 'allow_blank': False},
+            'password': {'write_only': True, 'required': True}
+        }
+
+    def validate_email(self, email: str):
+        if User.objects.filter(email=email).exclude(id=self.instance.id if self.instance else None).exists():
+            raise serializers.ValidationError("Um usuário com este email já existe.")
+        return True
--- teste_python/accounts/views.py
+++ teste_python/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
-# Create your views here.
+from . import services
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            if serializer.validate_email(serializer.validated_data["email"]):
+                try:
+                    user = services.create_user(serializer.validated_data["email"],
+                                                serializer.validated_data["first_name"],
+                                                serializer.validated_data["last_name"],
+                                                serializer.validated_data["password"])
+                except ValidationError as e:
+                    return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+                output_serializer = UserSerializer(user, context={'request': request})
+                return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
